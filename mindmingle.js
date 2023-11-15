43972434c9975e09da5a418a447e0bd6e6997bb2
Tatyana D <tdorcinvil2022@fau.edu>
2023-11-15 00:08:07 -0600

    Changed public => dist

--- mindmingle.js
+++ mindmingle.js
@@ -171,10 +171,10 @@ function authenticateToken(req, res, next) {
 
 // Serve Vue application
 // Make sure the path to 'dist' is correct
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'homePage.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'homePage.html'));
 });
 
 // Start server
