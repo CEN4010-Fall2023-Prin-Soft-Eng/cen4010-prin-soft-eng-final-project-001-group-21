f477c58213836e6d17063c2043a4c25f8723d378
Tatyana D <tdorcinvil2022@fau.edu>
2023-11-26 18:35:49 -0600

    Added CSS to sign up

--- src/components/SignupComponent.vue
+++ src/components/SignupComponent.vue
@@ -1,25 +1,26 @@
 <template>
-  <div>
-    <h2>Sign Up</h2>
-    <form @submit.prevent="signup">
+  <div class="signup-container">
+    <h2 class="center">Sign Up</h2>
+    <form @submit.prevent="signup" class="center">
       <label for="signup-username">Username:</label>
-      <input type="text" id="signup-username" v-model="username" required>
+      <input type="text" id="signup-username" v-model="username" class="form-input" required>
 
       <label for="signup-email">Email:</label>
-      <input type="email" id="signup-email" v-model="email" required>
+      <input type="email" id="signup-email" v-model="email" class="form-input" required>
 
       <label for="signup-password">Password:</label>
-      <input type="password" id="signup-password" v-model="password" required autocomplete="new-password">
+      <input type="password" id="signup-password" v-model="password" class="form-input" required autocomplete="new-password">
 
       <label for="signup-confirm-password">Confirm Password:</label>
-      <input type="password" id="signup-confirm-password" v-model="confirmPassword" required>
+      <input type="password" id="signup-confirm-password" v-model="confirmPassword" class="form-input" required>
 
-      <button type="submit">Sign Up</button>
+      <button type="submit" class="signup-button">Sign Up</button>
     </form>
-    <h2>Already have an account? <a href="/login">Login</a></h2>
+    <div class="center">
+      <h2>Already have an account? <a href="/login">Login</a></h2>
+    </div>
   </div>
 </template>
-
 
 <script>
 import axios from 'axios';
@@ -67,3 +68,59 @@ export default {
   }
 };
 </script>
+
+<style>
+    /* Basic styling for the navigation bar */
+    ul.navbar {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        background-color: #333;
+        overflow: hidden;
+    }
+
+    li.nav-item {
+        float: left;
+    }
+
+    li.nav-item a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    /* Style for the registration form container */
+    .signup-container {
+        margin: 20px auto;
+        padding: 20px;
+        border: 1px solid #ccc;
+        max-width: 300px;
+        background-color: #f9f9f9;
+    }
+
+    /* Style for form fields */
+    .form-input {
+        width: 100%;
+        padding: 10px;
+        margin: 5px 0;
+        box-sizing: border-box;
+    }
+
+    /* Style for the sign-up button */
+    .signup-button {
+        background-color: #428bca;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+        margin-top: 10px; /* Add some spacing between form fields and the sign-up button */
+    }
+
+    /* Center the form */
+    .center {
+        text-align: center;
+    }
+</style>
