7f4c6a45c16c1297e7a7663d4cec11aa10aef3be
Your Name <your_email@example.com>
2023-12-02 11:16:17 +0000

    ip

--- test-db.js
+++ test-db.js
@@ -1,12 +1,20 @@
-// test-db.js
-const db = require('./db/db-config');
+require('dotenv').config();
+const { Pool } = require('pg');
 
-db.raw('SELECT 1+1 AS result')
-  .then((result) => {
-    console.log('Database connection successful:', result);
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error('Database connection error:', err);
-    process.exit(1);
-  });
+// Use the environment variables from your .env file
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.query('SELECT NOW()', (error, results) => {
+  if (error) {
+    console.error('Connection test failed:', error);
+  } else {
+    console.log('Connection test succeeded:', results.rows);
+  }
+  pool.end(); // Make sure to close the connection after the test
+});
